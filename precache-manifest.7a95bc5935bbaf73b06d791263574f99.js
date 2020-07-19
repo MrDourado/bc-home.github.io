7a694e86c909eadca3bc0115bcd9621a2d2afeb8
MrDourado <fernando.dourado@gmail.com>
2020-07-19 15:10:10 -0300

    Updates

---
+++ precache-manifest.7a95bc5935bbaf73b06d791263574f99.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bcdd37aa3505dea38cd2bcf79f75f49",
+    "url": "/bc-home.github.io/index.html"
+  },
+  {
+    "revision": "4a70d407d46e3c9acd40",
+    "url": "/bc-home.github.io/static/css/2.4c00aa32.chunk.css"
+  },
+  {
+    "revision": "53839bedcf6e002ce067",
+    "url": "/bc-home.github.io/static/css/main.a3402591.chunk.css"
+  },
+  {
+    "revision": "4a70d407d46e3c9acd40",
+    "url": "/bc-home.github.io/static/js/2.42f7bf36.chunk.js"
+  },
+  {
+    "revision": "53839bedcf6e002ce067",
+    "url": "/bc-home.github.io/static/js/main.513fad56.chunk.js"
+  },
+  {
+    "revision": "661c56f26ba0e0efedf9",
+    "url": "/bc-home.github.io/static/js/runtime~main.86c60f05.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/bc-home.github.io/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/bc-home.github.io/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/bc-home.github.io/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/bc-home.github.io/static/media/slick.f97e3bbf.svg"
+  }
+]);
